2fe7ec9ef278a18a54968fc312323248afbf6507
K4YT3X <k4t@protonmail.com>
2017-07-06 16:42:31 -0400

    Version 2.4.1: Added avalon framework installation wizard

--- scutum.py
+++ scutum.py
@@ -40,7 +40,6 @@ For tutorial please look at the Github Page: https://github.com/K4YT3X/SCUTUM
 """
 from __future__ import print_function
 import argparse
-import avalon_framework as avalon
 import datetime
 import ipaddress
 import os
@@ -49,8 +48,46 @@ import struct
 import subprocess
 import time
 
+try:
+    import avalon_framework as avalon
+except ImportError:
+    while True:
+        install = input('\033[31m\033[1mAVALON Framework not installed! Install now? [Y/n] \033[0m')
+        if len(install) == 0 or install[0].upper() == 'Y':
+            try:
+                if os.path.isfile('/usr/bin/pip3'):
+                    print('Installing using method 1')
+                    os.system('pip3 install avalon_framework')
+                elif os.path.isfile('/usr/bin/wget'):
+                    print('Installing using method 2')
+                    os.system('wget -O - https://bootstrap.pypa.io/get-pip.py | python3')
+                    os.system('pip3 install avalon_framework')
+                else:
+                    print('Installing using method 3')
+                    import urllib.request
+                    content = urllib.request.urlopen('https://bootstrap.pypa.io/get-pip.py')
+                    with open('/tmp/get-pip.py', 'w') as getpip:
+                        getpip.write(content.read().decode())
+                        getpip.close()
+                    os.system('python3 /tmp/get-pip.py')
+                    os.system('pip3 install avalon_framework')
+                    os.remove('/tmp/get-pip.py')
+            except Exception as e:
+                print('\033[31mInstallation failed!: ' + str(e))
+                print('Please check your Internet connectivity')
+                exit(0)
+            print('\033[32mInstallation Succeed!\033[0m')
+            print('\033[32mPlease restart the program\033[0m')
+            exit(0)
+        elif install[0].upper() == 'N':
+            print('\033[31m\033[1mSCUTUMM requires avalon framework to run!\033[0m')
+            print('\033[33mAborting..\033[0m')
+            exit(0)
+        else:
+            print('\033[31m\033[1mInvalid Input!\033[0m')
+
 LOGPATH = '/var/log/scutum.log'
-VERSION = '2.4.1'
+VERSION = '2.4.2'
 
 
 # -------------------------------- Functions --------------------------------
