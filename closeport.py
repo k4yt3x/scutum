832efb280ba1448dbf3d9949af7ba95e74cdb04d
K4YT3X <k4t@protonmail.com>
2018-04-25 00:27:23 -0400

    v1.2: fixed import error

--- closeport.py
+++ closeport.py
@@ -4,22 +4,22 @@
 Name: SCUTUM Easy TCP: closeport
 Author: K4YT3X
 Date Created: Sep 15, 2017
-Last Modified: Sep 28, 2017
+Last Modified: April 25, 2018
 
 Description: This script closes oprts on UFW Firewall
 
 This class is migrated from Project: DefenseMatrix
 
-Version 1.1
+Version 1.2
 """
 
-from iptables import Ufw
-from logger import Logger
+from ufw import Ufw
 import avalon_framework as avalon
 import sys
+LOGPATH = '/var/log/scutum.log'
 
-log = Logger()
-ufwctrl = Ufw(log)
+log = open(LOGPATH, 'a+')
+ufwctrl = Ufw(log=log)
 try:
     ports = []
     for port in sys.argv[1:]:
