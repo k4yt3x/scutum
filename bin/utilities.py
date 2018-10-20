bb68582927ffddf24b893acf9cf4323b5097fa95
K4YT3X <k4yt3x@k4yt3x.com>
2018-10-20 02:26:38 -0400

    1.0.1 fixing package installer IO issues

--- bin/utilities.py
+++ bin/utilities.py
@@ -7,13 +7,14 @@ Date Created: October 19, 2018
 Last Modified: October 19, 2018
 
 Description: This class contains some useful utilities.
-
-Version 1.0.0
 """
 from avalon_framework import Avalon
+import os
 import shutil
 import subprocess
 import sys
+
+VERSION = '1.0.1'
 
 
 class Utilities:
@@ -22,29 +23,33 @@ class Utilities:
     This class contains a number of utility tools.
     """
 
-    def execute(command, input_value=''):
+    def execute(command, std_in='', std_out=subprocess.PIPE, std_err=subprocess.PIPE):
         """ Execute a system command and return the output
         """
-        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=sys.stdout)
-        return process.communicate(input=input_value)[0].decode().split('\n')
+        if type(std_in) is str:
+            process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=std_out, stderr=std_err)
+            return process.communicate(input=std_in)[0].decode().split('\n')
+        else:
+            process = subprocess.Popen(command, stdin=sys.stdin, stdout=std_out, stderr=std_err)
+            return process.communicate()[0].decode().split('\n')
 
     def install_package(package):
         """ Install a package using system package manager
         """
+        Avalon.warning('If the installation is unsuccessful, you should consider updating the package manager cache', log=False)
         if Avalon.ask('Install {}?'.format(package), True):
-            if shutil.which('/usr/bin/apt') is not None:
-                Utilities.execute(['apt', 'update', '&&', 'apt', 'install', package, '-y'])  # install arptables with apt
+            if shutil.which('apt-get'):
+                os.system('apt-get install {} -y'.format(package))
                 return True
-            elif shutil.which('/usr/bin/yum') is not None:
-                Utilities.execute(['yum', 'install', package, '-y'])  # install arptables with yum
+            elif shutil.which('yum'):
+                os.system('yum install {} -y'.format(package))
                 return True
-            elif shutil.which('/usr/bin/pacman') is not None:
-                Utilities.execute(['pacman', '-S', package, '--noconfirm'])  # install arptables with pacman
+            elif shutil.which('pacman'):
+                os.system('pacman -S {} --noconfirm'.format(package))
                 return True
             else:
                 Avalon.error('Sorry, we can\'t find a package manager that we currently support. Aborting..')
                 Avalon.error('Currently Supported: apt, yum, pacman')
-                Avalon.error('Please come to SCUTUM\'s github page and comment if you know how to add support to another package manager')
                 return False
         else:
             return False
